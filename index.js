48895c50c158ea257247923af6c6193368b07164
Syed Ateeq <cs172011@dsu.edu.pk>
2022-07-02 14:17:59 +0500

    Added neccessary changes.

---
+++ index.js
@@ -0,0 +1,25 @@
+let count = 0
+let countEl = document.getElementById("count")
+
+let saveEl = document.getElementById("save-el")
+
+function increment()
+{
+    count += 1
+    countEl.innerText = count
+   
+}
+
+function save(){
+    
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    
+   
+    
+}
+
+
+
